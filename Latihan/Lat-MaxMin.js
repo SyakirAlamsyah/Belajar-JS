bba70d1d24b85fa0b0655cc2e91a16060fa68bbc
dep0r <syakiralamsyah49@gmail.com>
2026-05-29 00:48:52 +0700

    Latihan Max Min

---
+++ Latihan/Lat-MaxMin.js
@@ -0,0 +1,21 @@
+function maxmin(arr){
+
+    var result = {
+        max : arr[0],
+        min : arr[0]
+    }
+
+    for(var i=0; i < arr.length; i++){
+        
+        if(arr[i] > result.max){
+            result.max = arr[i]
+        }
+        if (arr[i] < result.min){
+            result.min = arr[i]
+        }   
+    }
+
+    return result
+}
+
+console.log(maxmin([10, 3, 5, 12, 7]))
